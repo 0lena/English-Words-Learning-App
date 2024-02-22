688c17c147147cd0c1703ed81e097eacbe687801
0lena <78179171+0lena@users.noreply.github.com>
2024-02-22 18:18:51 -0500

    Add files via upload

--- English Learning Game/service.py
+++ English Learning Game/service.py
@@ -1,34 +1,36 @@
-from storage import *
+
+from storage import DictItem, Database
 
 class Service:
-    def __init__(self, dictionary):
-        self.dictionary = dictionary
+    def __init__(self, db: Database):
+        self.db = db
+        self.username = "unknown"
 
     def add_new_word(self, topic, word, meaning, pos):
         new_item = DictItem(topic=topic, word=word, meaning=meaning, pos=pos)
         new_item.validate_input()
 
-        # Storage instance to write this new word to the file
-        items = self.dictionary.read()
-        for item in items:
-            if item.word == new_item.word:
-                raise RuntimeError(f"Word {item.word} already exists")
-        items.append(new_item)
-        self.dictionary.write(items)
-
+        self.db.add_new_word(self.username, new_item)
 
     def get_sorted_topics(self):
-        topics = set()
-        for word in self.dictionary.read():
-            topics.add(word.topic)
+        topics = self.db.get_topic_names(self.username)
         return list(sorted(topics))
 
     # Filter words by specific topic
     def get_words_by_topic(self, topic):
-        items = self.dictionary.read()
-        words_by_topic = [] # DictItem objects sorted by chosen topic
-        for item in items:
-            if item.topic == topic:
-                words_by_topic.append(item)
-        return words_by_topic
+        return self.db.get_words(self.username, topic)
 
+    #Settings for goal
+    def update_goal(self, new_goal):
+        settings = self.db.get_settings(self.username)
+        settings["goal"] = new_goal
+        self.db.update_settings(self.username, settings)
+
+    def get_progress(self):
+        settings = self.db.get_settings(self.username)
+        goal = int(settings.get("goal", 0))
+
+        words_learned = 0
+        for topic in self.db.get_topic_names(self.username):
+            words_learned += len(self.db.get_words(self.username, topic))
+        return words_learned, goal
